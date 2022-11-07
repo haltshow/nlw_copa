b8488142f695e4065c8d9fc6facf70eb715c7cb0
Gabriel Abreu <gabrielabreualves@gmail.com>
2022-11-06 22:56:18 -0300

    corrigindo erros

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,12 @@
+import { Text, Center } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+        
+        <Text color="white" fontSize={36}> 
+          SignIn
+        </Text>
+      </Center>
+    )
+}
