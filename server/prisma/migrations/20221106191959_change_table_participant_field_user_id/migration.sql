acf574e8d330fec8de830dd9e2722ec0418c71b3
Gabriel Abreu <gabrielabreualves@gmail.com>
2022-11-06 22:24:10 -0300

    commit inicial

---
+++ server/prisma/migrations/20221106191959_change_table_participant_field_user_id/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Participant" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT,
+    "poolId" TEXT NOT NULL,
+    CONSTRAINT "Participant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Participant_poolId_fkey" FOREIGN KEY ("poolId") REFERENCES "Pool" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Participant" ("id", "poolId", "userId") SELECT "id", "poolId", "userId" FROM "Participant";
+DROP TABLE "Participant";
+ALTER TABLE "new_Participant" RENAME TO "Participant";
+CREATE UNIQUE INDEX "Participant_userId_poolId_key" ON "Participant"("userId", "poolId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
